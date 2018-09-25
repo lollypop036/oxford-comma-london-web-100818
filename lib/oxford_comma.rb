a92d1fc22e080593ad3ad33bff2df75b4d5c3988
Lola Onanuga <slonanuga@gmail.com>
2018-09-25 18:44:11 +0000

    complete

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+if array.size == 1
+  array.join(", ")
+  
+elsif array.size == 2
+  array.join(" and ")
+   
+elsif array.size >= 3
+array.insert(-2, "and")
+array = array.join(", ")
+array.reverse.sub(",","").reverse
+
+end
 
 end
